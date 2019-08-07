97ce3252132ddc5858666e68def3bb3301646253
Triumph <archie200@icloud.com>
2019-08-07 11:58:09 -0500

    routes/api

--- routes/api/books.js
+++ routes/api/books.js
@@ -9,7 +9,7 @@ router.route('/')
     .get(booksController.findAll);
 
 // matches with /api/books/:id
-router.route(':id')
+router.route('/:id')
     .get(booksController.findById)
     .post(booksController.addBook)
     .put(booksController.updateBook)
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 // file imports
 const bookRoutes = require('./books.js')
-const anotherThingRoutes = require('./anotherThing.js')
+// const anotherThingRoutes = require('./anotherThing.js')
 
 // routes
 // this is /api/books
 router.use('/books', bookRoutes);
 // this is /api/anotherThing
-router.use('/anotherThing', anotherThingRoutes);
+// router.use('/anotherThing', anotherThingRoutes);
 
 // any other route send them to build index.html
 router.use((req, res) => {
