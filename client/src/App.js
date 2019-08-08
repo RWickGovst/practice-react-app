ceeadd78f85e795ad53c7559c28389f8c7031b3d
Triumph <archie200@icloud.com>
2019-08-08 09:01:49 -0500

    added display

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-
-// react router
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-//pages for router
-// import Delete from './pages/Delete';
-import FindId from './pages/FindId';
-import Home from './pages/Home';
+import React from "react";
+import HelloBootstrap from "./components/HelloBootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route exact path='/find' component={FindId} />
-        <Route exact path='/home' component={Home} />
-        {/* <Route exact path='/delete/:id' component={Delete} /> */}
-
-      </Router>
-    </div>
-  );
+  return <HelloBootstrap />;
 }
 
 export default App;
