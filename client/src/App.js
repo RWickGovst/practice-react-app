cd6e1c25502bb07d31f53fd982e31f520471b936
Triumph <archie200@icloud.com>
2019-08-08 08:47:38 -0500

    trying to display

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import './App.css';
 
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //pages for router
 // import Delete from './pages/Delete';
 import FindId from './pages/FindId';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path='/find' component={FindId} />
+        <Route exact path='/home' component={Home} />
         {/* <Route exact path='/delete/:id' component={Delete} /> */}
 
       </Router>
